9abb076e24f68bac0f239eb1513284d31e1c8b46
woranard Norkerw <zankzii19@gmail.com>
2019-07-13 14:32:47 +0700

    เพิ่มหน้าเทล

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const port = 3000
 app.get('/', (req, res) => res.send('Woranard'))
 app.get('/about', (req, res) => res.send('หน้า node'))
 app.get('/contact', (req, res) => res.send('หน้า เกี่ยวกับ'))
-
+app.get('/contact', (req, res) => {
+    res.json({message: "test"})
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
